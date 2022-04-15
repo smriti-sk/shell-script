f0852b5ba60cc6cdabbb7f4bbc29c7d429a2b72d
smriti-sk <ksmritie@gmail.com>
2022-04-16 00:00:19 +0530

    First shell script

---
+++ firstShellScript.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+
+# This is a comment,thus this line Will not be executed
+# 'echo' will output the text
+# 'read' will take input from user
+
+echo "Enter your name:"
+read n
+echo "Your name is $n"
+
+
+
